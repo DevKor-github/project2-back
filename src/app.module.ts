bf77760255c4ed6cd5478233a949f1f4f0c23c26
overthestream <njhnjh02@naver.com>
2023-08-15 11:30:32 +0900

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ConfigModule } from '@nestjs/config';
       synchronize: true,
     }),
     PostModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
